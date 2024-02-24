85b1936c112caf6505eae54b04009ac1ec64fb7d
Mario <mario.michaeljs21@gmail.com>
2024-02-24 20:08:45 +0700

    remove unimportant part such as auto-generated comment

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -14,7 +14,6 @@ public class CarServiceImpl implements CarService{
     private CarRepository carRepository;
     @Override
     public Car create(Car car) {
-        // TODO Auto-generated method stub
         carRepository.create(car);
         return car;
     }
@@ -31,12 +30,10 @@ public class CarServiceImpl implements CarService{
     }
     @Override
     public void update(String carId, Car car) {
-        // TODO Auto-generated method stub
         carRepository.update(carId, car);
     }
     @Override
     public void deleteCarById(String carId) {
-        // TODO Auto-generated method stub
         carRepository.delete(carId);
     }
 }
